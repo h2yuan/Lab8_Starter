808913c735160bc1fdf795cc77a2e8c7752faf6c
Hao Yuan <h2yuan@ucsd.edu>
2021-11-10 17:48:57 -0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+
+1) ANS: It should be the first choice, that we debug the code every time we push it into github.
+2) ANS: yes
+3) 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -23,7 +23,7 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
+    console.log(`Checking product item` + i + `/${prodItems.length}`);
     // Grab the .data property of <product-items> to grab all of the json data stored inside
     data = await prodItems[0].getProperty('data');
     // Convert that property to JSON
@@ -34,6 +34,20 @@ describe('Basic user flow for Website', () => {
     if (plainValue.image.length == 0) { allArePopulated = false; }
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
+
+    const prodItems = await page.$$('product-item');
+
+    for (let i = 0; i <= 19; i++) {
+      console.log(`Checking product item 1/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[i].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; } 
+    }
 
     // TODO - Step 1
     // Right now this function is only checking the first <product-item> it found, make it so that
