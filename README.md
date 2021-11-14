afc5c577ad7ebdc5199261d7cc13e2f8319241b7
Hao Yuan <h2yuan@ucsd.edu>
2021-11-14 04:51:03 -0800

    done

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Lab 8 - Starter
 
 1) ANS: It should be the first choice, that we debug the code every time we push it into github.
-2) ANS: yes
-3) 
+2) ANS: No.
+3) ANS: No, given the cons that 'Cannot test how these individual components interact with each other on an application/feature level.'
+4) ANS: Yes, only to test the max message length feature is a small part of testing. Using unit test should be much easier and quicker.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,103 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('isPhoneNumber Test1', () => {
+    // TODO
+    expect(functions.isPhoneNumber('(888)888-888-8888')).toBe(true);
+});
+
+test('isPhoneNumber Test2', () => {
+    // TODO
+    expect(functions.isPhoneNumber('888-888-8888')).toBe(true);
+});
+
+test('isPhoneNumber Test3', () => {
+    // TODO
+    expect(functions.isPhoneNumber('(888)888-88-8888')).toBe(false);
+});
+
+test('isPhoneNumber Test4', () => {
+    // TODO
+    expect(functions.isPhoneNumber('(88)888-888')).toBe(false);
+});
+
+test('isEmail Test1', () => {
+    // TODO
+    expect(functions.isEmail('aaa@aaa.aaa')).toBe(true);
+});
+
+test('isEmail Test2', () => {
+    // TODO
+    expect(functions.isEmail('a@a.aa')).toBe(true);
+});
+
+test('isEmail Test3', () => {
+    // TODO
+    expect(functions.isEmail('aaaaaaaa')).toBe(false);
+});
+
+test('isEmail Test4', () => {
+    // TODO
+    expect(functions.isEmail('aaa@aaa.aaaaaaa')).toBe(false);
+});
+
+test('isStrongPassword Test1', () => {
+    // TODO
+    expect(functions.isStrongPassword('aaaaaaaaaaaa')).toBe(true);
+});
+
+test('isStrongPassword Test2', () => {
+    // TODO
+    expect(functions.isStrongPassword('aaaaa')).toBe(true);
+});
+
+test('isStrongPassword Test3', () => {
+    // TODO
+    expect(functions.isStrongPassword('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa')).toBe(false);
+});
+
+test('isStrongPassword Test4', () => {
+    // TODO
+    expect(functions.isStrongPassword('aa')).toBe(false);
+});
+
+test('isDate Test1', () => {
+    // TODO
+    expect(functions.isDate('0/0/0000')).toBe(true);
+});
+
+test('isDate Test2', () => {
+    // TODO
+    expect(functions.isDate('00/00/0000')).toBe(true);
+});
+
+test('isDate Test3', () => {
+    // TODO
+    expect(functions.isDate('0/0/00000')).toBe(false);
+});
+
+test('isDate Test4', () => {
+    // TODO
+    expect(functions.isDate('000/0/0000')).toBe(false);
+});
+
+test('isHexColor Test1', () => {
+    // TODO
+    expect(functions.isHexColor('aaa')).toBe(true);
+});
+
+test('isHexColor Test2', () => {
+    // TODO
+    expect(functions.isHexColor('111111')).toBe(true);
+});
+
+test('isHexColor Test3', () => {
+    // TODO
+    expect(functions.isHexColor('')).toBe(false);
+});
+
+test('isHexColor Test4', () => {
+    // TODO
+    expect(functions.isHexColor('a1a1a1a1a1a1a1')).toBe(false);
+});
